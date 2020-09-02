c130727d2cd77766e3e857c1b6da744cfbb1b7dd
Walter Nunes <jnunes.walter@gmail.com>
2020-09-02 01:03:24 -0300

    Add project Readble

---
+++ Readble/client/src/reducers/PostReducer.js
@@ -0,0 +1,36 @@
+import { GET_POST, GET_ALL_POSTS, VOTE_POST, CREATE_POST, EDIT_POST, DELETE_POST, ADD_COMMENT, SUB_COMMENT } from '../actions/';
+
+export default function (state = {}, action) {
+    switch (action.type) {
+
+      case GET_POST:
+        return [action.posts]
+
+      case GET_ALL_POSTS:
+        return action.posts
+
+      case VOTE_POST:
+        return state.map((post) => post.id === action.posts.id ? action.posts : post )
+
+      case CREATE_POST:
+        return [...state, action.posts];
+
+      case EDIT_POST:
+        return state.map((post) => post.id === action.posts.id ?
+            {...post, title: action.posts.title, body:action.posts.body} : post)
+
+      case DELETE_POST:
+        return state.filter((post) => post.id !== action.posts.id)
+
+      case ADD_COMMENT:
+        return state.map((post) => post.id === action.id ?
+            {...post, commentCount: post.commentCount + 1} : post)
+
+      case SUB_COMMENT:
+        return state.map((post) => post.id === action.id ?
+            {...post, commentCount: post.commentCount - 1} : post)
+
+      default:
+        return state;
+    }
+}
